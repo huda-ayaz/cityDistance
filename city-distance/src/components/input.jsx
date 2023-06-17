3ba2180dc3ec0bcaf2249507045659cc45a724fd
jons-repo <s.jonathan4252@gmail.com>
2023-06-16 20:35:53 -0400

    Added two inputs for zipcodes and compute distance logic

---
+++ city-distance/src/components/input.jsx
@@ -0,0 +1,42 @@
+import React, {useState} from "react";
+import axios from "axios";
+
+const Input = () => {
+    const [zipCode, setZipCode] = useState("");
+    const [zipCode2, setZipCode2] = useState("");
+    const [distance, setDistance] = useState("");
+    const [unit, setUnit] = useState("");
+
+    async function getDistance() {
+        try {
+            const distance = await axios.get(`https://zip-api.eu/api/v1/distance/US-${zipCode}/US-${zipCode2}/${unit}`);
+            setDistance(distance.data.distance);
+        } catch(error) {
+            console.error(error);
+        }
+    }
+
+    const handleInputChange = (event, setState) => {
+        setState(event.target.value);
+    };
+
+    const selectValue = (event) => {
+        setUnit(event.target.value);
+    }
+    
+    return (
+      <div>
+        <input value={zipCode} onChange={(event) => handleInputChange(event, setZipCode)} />
+        <input value={zipCode2} onChange={(event) => handleInputChange(event, setZipCode2)} />
+        <select onChange={selectValue}>
+                <option value="km">km</option>
+                <option value="mi">mi</option>
+        </select>
+       
+        <button onClick={getDistance}>Compute Distance</button>
+        <p key={distance.url}>{distance} {unit}</p>
+      </div>
+    );
+};
+
+export default Input;
