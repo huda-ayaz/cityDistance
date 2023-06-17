8e58f43b72c95e524d1dad90c4e8384827443b98
Huda Ayaz <hudaayaz21@gmail.com>
2023-06-17 17:19:39 -0400

    Updates

---
+++ precache-manifest.768475a4332256432bdbb47049f63124.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c8f4b7dda9305de847d",
+    "url": "/cityDistance/static/css/main.9ac2ac99.chunk.css"
+  },
+  {
+    "revision": "9c8f4b7dda9305de847d",
+    "url": "/cityDistance/static/js/main.9c8f4b7d.chunk.js"
+  },
+  {
+    "revision": "d0acc314c0a6c805f44b",
+    "url": "/cityDistance/static/js/1.d0acc314.chunk.js"
+  },
+  {
+    "revision": "2fb6d220f49d686fb711",
+    "url": "/cityDistance/static/js/2.2fb6d220.chunk.js"
+  },
+  {
+    "revision": "9703c20350b624d14ea9",
+    "url": "/cityDistance/static/js/runtime~main.9703c203.js"
+  },
+  {
+    "revision": "3a5abf850cb5bbe84b9e784449a67887",
+    "url": "/cityDistance/index.html"
+  }
+];
